4d62092a7db87e8db4e2c60fa9600a212c888124
Jonathan Wahrenberg <jonathan@wahrenberg.com>
2026-04-14 01:01:21 +0200

    feat: Add assets. finished level 2 and 3. Updated level 1 map. Added death zone

---
+++ scripts/level_2.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var exit_door: StaticBody3D = $NavigationRegion3D/Level2_Map/Map/Props/ExitDoor
+
+var exit_open = false
+
+func _process(delta: float) -> void:
+	var count = get_tree().get_nodes_in_group("enemy").size()
+	if (not exit_open and count == 0):
+		exit_open = true
+		exit_door.queue_free()
